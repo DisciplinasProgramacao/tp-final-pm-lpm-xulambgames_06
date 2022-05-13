60b46695f5d6d2b59cfe055b7488c2e44ad39aaf
Saullo Bretas Silva <saullo.silva303@gmail.com>
2022-05-13 12:21:15 -0300

    Initial menu implementation

--- src/main/java/tp_final_pm_lpm/xulambgames/Main.java
+++ src/main/java/tp_final_pm_lpm/xulambgames/Main.java
@@ -1,7 +1,33 @@
 package tp_final_pm_lpm.xulambgames;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final int EXIT_OPTION = 0;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var menuString = """
+                0 - Sair
+                1 - Cadastrar cliente
+                2 - Historico cliente
+                3 - Cadastrar jogo
+                4 - Cadastrar compra
+                """;
+
+        var scanner = new Scanner(System.in);
+        int option = -1;
+        while(option != EXIT_OPTION) {
+            System.out.println(menuString);
+            System.out.print("Opção: ");
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 0 -> System.out.println("Sair");
+                case 1 -> System.out.println("Cadastrar cliente");
+                case 2 -> System.out.println("Historico cliente");
+                case 3 -> System.out.println("Cadastrar jogo");
+                case 4 -> System.out.println("Cadastrar compra");
+            }
+        }
     }
 }
